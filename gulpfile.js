ebb25c43ace2a6c5981608ccd8a81ff76247ff60
Rohan Parkes <rparkes@email.com>
2020-09-28 16:44:08 +1000

    Gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const env = require("gulp-env");
 gulp.task("test", function () {
   env({ vars: { NODE_ENV: "test" } });
 
-  return gulp.src("test/**/*.js").pipe(mocha);
+  return gulp.src("test/**/*.js").pipe(mocha());
 });
 
-gulp.task("default", ["test"]);
+gulp.task("default", gulp.series("test"));
