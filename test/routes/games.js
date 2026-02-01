e81e664caabc2881beed64ad81ca432a7170eb9b
Rohan Parkes <rparkes03@gmail.com>
2026-02-02 07:33:15 +1100

    Fixed two tests.

--- test/routes/games.js
+++ test/routes/games.js
@@ -4,42 +4,42 @@ import bodyParser from 'body-parser';
 import { expect } from 'chai';
 import express from 'express';
 import request from 'supertest';
-import mongoose from '../../src/config/mongoose.js';
+import dbase from '../../src/config/mongoose.js';
 import games from '../../src/routes/games.js';
 import gamesService from '../../src/services/games.js';
-
-// const TEST_PORT = 5000;
+import usersService from '../../src/services/users.js';
 
 describe('/games', () => {
-  // let server;
-  let userId, agent, gs, app;
+  let userId, agent, g_service, u_service, app;
   userId = 'test-user-id';
 
-  before(() => {
-    mongoose
+  before((done) => {
+    dbase
       .then((mongoose) => {
-        gs = gamesService(mongoose);
-        console.log('db created ...');
+        app = express();
+        app.use(bodyParser.json());
+        app.use((req, _res, next) => {
+          // users middleware stub.
+          req.user = { id: userId };
+          next();
+        });
+        gamesService(mongoose).then((gs) => {
+          g_service = gs;
+          usersService.then((us) => {
+            u_service = us;
+            app.use('/games', games(g_service, u_service));
+            done();
+          });
+        });
       })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    app = express();
-    app.use(bodyParser.json());
-    app.use((req, _res, next) => {
-      // users middleware stub.
-      req.user = { id: userId };
-      next();
-    });
-
-    app.use('/games', games);
+      .catch(done);
   });
 
   beforeEach(function (done) {
     agent = request.agent(app);
 
-    gs.availableTo('non-existent-user') // return all games.
+    g_service
+      .availableTo('non-existent-user') // return all games.
       .then((games) => games.map((game) => game.deleteOne())) // An array of Promises of delete operations.
       .then((gamesRemoved) => Promise.all(gamesRemoved)) // Perform all delete operations.
       .then(() => done(), done);
@@ -47,7 +47,7 @@ describe('/games', () => {
 
   describe('/:id DELETE', () => {
     it('should allow users to delete their own games', (done) => {
-      gs.create(userId, 'test').then((game) => {
+      g_service.create(userId, 'test').then((game) => {
         agent
           .delete('/games/' + game.id)
           .expect(200)
@@ -55,7 +55,8 @@ describe('/games', () => {
             if (err) {
               done(err);
             } else {
-              gs.createdBy(userId)
+              g_service
+                .createdBy(userId)
                 .then((createdGames) => {
                   expect(createdGames).to.be.empty;
                 })
@@ -66,7 +67,7 @@ describe('/games', () => {
     });
 
     it('should not allow users to delete games that they did not set', (done) => {
-      gs.create('another-user-id', 'test').then((game) => {
+      g_service.create('another-user-id', 'test').then((game) => {
         agent
           .delete('/games/' + game.id)
           .expect(403)
@@ -74,7 +75,8 @@ describe('/games', () => {
             if (err) {
               done(err);
             } else {
-              gs.get(game.id)
+              g_service
+                .get(game.id)
                 .then((createdGame) => {
                   expect(createdGame).ok;
                 })
@@ -85,7 +87,7 @@ describe('/games', () => {
     });
 
     it('should return a 404 for requests to delete a game that no longer exists', (done) => {
-      gs.create(userId, 'test').then((game) => {
+      g_service.create(userId, 'test').then((game) => {
         agent
           .delete(`/games/${game.id}`)
           .expect(200)
--- test/services/games.js
+++ test/services/games.js
@@ -1,57 +1,50 @@
-"use strict";
+/* eslint-disable no-undef */
 
-const expect = require("chai").expect;
-// const debug = require("debug")("hangman:services:test");
+import { expect } from 'chai';
+import dbase from '../../src/config/mongoose.js';
+import gservice from '../../src/services/games.js';
 
-describe("Game service", () => {
-  const firstUserId = "user-id-1";
-  const secondUserId = "user-id-2";
-
+describe('Game service', () => {
+  const firstUserId = 'user-id-1';
+  const secondUserId = 'user-id-2';
   let service;
+
   before((done) => {
-    require("../../src/config/mongoose.js")
+    dbase
       .then((mongoose) => {
-        service = require("../../src/services/games.js")(mongoose);
-        done();
+        gservice(mongoose).then((gs) => {
+          service = gs;
+          done();
+        });
       })
       .catch(done);
   });
 
   beforeEach(function (done) {
     service
-      .availableTo("non-existent-user")
-      .then((games) => games.map((game) => game.remove()))
+      .availableTo('non-existent-user')
+      .then((games) => games.map((game) => game.deleteOne()))
       .then((gamesRemoved) => Promise.all(gamesRemoved))
       .then(() => done(), done);
   });
 
-  /*
-  after(function (done) {
-    require("../../src/config/mongoose.js")
-      .default.then((mongoose) => {
-        mongoose.disconnect();
-      })
-      .then(done);
-  });
-  */
-
-  describe("list of available games", () => {
-    it("should include games set by other users", (done) => {
+  describe('list of available games', () => {
+    it('should include games set by other users', (done) => {
       service
-        .create(firstUserId, "hello")
-        .then(() => service.availableTo(secondUserId))
+        .create(firstUserId, 'testing') // First user creates single game.
+        .then(() => service.availableTo(secondUserId)) // Game will become available to second user.
         .then((games) => {
-          expect(games.length).to.equal(1);
+          expect(games.length).to.equal(1); // Expect (1) game.
           let game = games[0];
           expect(game.setBy).to.equal(firstUserId);
-          expect(game.word).to.equal("HELLO");
+          expect(game.word).to.equal('TESTING');
         })
         .then(done, done);
     });
 
-    it("should not include games set by the same user", (done) => {
+    it('should not include games set by the same user', (done) => {
       service
-        .create(firstUserId, "testing")
+        .create(firstUserId, 'testing')
         .then(() => service.availableTo(secondUserId))
         .then((games) => {
           expect(games.length).to.equal(1);
