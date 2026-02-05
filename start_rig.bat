0f20eb131f27947d433ef5e63d56fb33936154be
Rohan Parkes <rparkes03@gmail.com>
2026-02-06 08:44:02 +1100

    Added chat server.

--- start_rig.bat
+++ start_rig.bat
@@ -1,6 +1,6 @@
 @echo off
-@REM START /B redis-server
-START /B mongod --dbpath C:\data\mongodb
+REM START /B redis-server
+REM START /B mongod --dbpath C:\data\mongodb
 set MONGODB_URL=mongodb://10.211.55.2/hangman
 set REDIS_URL=redis://10.211.55.2:6379/
 SLEEP 2
