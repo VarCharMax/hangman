c50c8d6626f93f7caf00d2ed00b7685c136ae2a8
Rohan Parkes <rparkes03@gmail.com>
2026-02-11 08:34:48 +1100

    Added reverse sort paramter.

--- src/services/users.js
+++ src/services/users.js
@@ -9,19 +9,24 @@ export default redisClient
       setUserName: (userId, name) => redis.set(`user:${userId}:name`, name),
       recordWin: (userId) => redis.zIncrBy('user:wins', 1, userId),
       getTopPlayers: () =>
-        redis.zRangeWithScores('user:wins', 0, 2, 'rev').then((interleaved) => {
-          if (interleaved.length === 0) {
-            return [];
-          }
-          let userIds = interleaved.map((user) => `user:${user.value}:name`);
-          return redis.mGet(userIds).then((names) =>
-            names.map((username, index) => ({
-              name: username,
-              userId: interleaved[index].value,
-              wins: parseInt(interleaved[index].score),
-            }))
-          );
-        }),
+        redis
+          .zRangeWithScores('user:wins', 0, 2, {
+            REV: true,
+          })
+          .then((interleaved) => {
+            if (interleaved.length === 0) {
+              return [];
+            }
+            // Sort by score property.
+            let userIds = interleaved.map((user) => `user:${user.value}:name`);
+            return redis.mGet(userIds).then((names) =>
+              names.map((username, index) => ({
+                name: username,
+                userId: interleaved[index].value,
+                wins: parseInt(interleaved[index].score),
+              }))
+            );
+          }),
       getRanking: async (userId) => {
         return Promise.all([
           redis.zRevRank('user:wins', userId),
