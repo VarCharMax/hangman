39b545caec0bff006c841c71ff3b51cbd9104319
Rohan Parkes <rparkes03@gmail.com>
2026-02-11 11:01:31 +1100

    Fixed unused params.

--- src/classes/redisClientMock.js
+++ src/classes/redisClientMock.js
@@ -15,49 +15,49 @@ export class redisClientMock extends EventEmitter {
     this.emit('ready', () => {});
   }
   zRangeWithScores(name, start, end, _param1) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, _reject) => {
       let returnVal = this.#redisClientTmp.zrange(name, start, end, 'REV', 'WITHSCORES');
       resolve(returnVal);
     });
   }
   get(name) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, _reject) => {
       let returnVal = this.#redisClientTmp.get(name);
       resolve(returnVal);
     });
   }
   set(key, val) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, _reject) => {
       let returnVal = this.#redisClientTmp.set(key, val);
       resolve(returnVal);
     });
   }
   zIncrBy(key, val, id) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, _reject) => {
       let returnVal = this.#redisClientTmp.zincrby(key, val, id);
       resolve(returnVal);
     });
   }
   mGet(ids) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, _reject) => {
       let returnVal = this.#redisClientTmp.mget(ids);
       resolve(returnVal);
     });
   }
   zRevRank(val, id) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, _reject) => {
       let returnVal = this.#redisClientTmp.zrevrank(val, id);
       resolve(returnVal);
     });
   }
   zScore(key, user1, user2) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, _reject) => {
       let returnVal = this.#redisClientTmp.zscore(key, user1, user2);
       resolve(returnVal);
     });
   }
   flushDb() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, _reject) => {
       let returnVal = this.#redisClientTmp.flushdb();
       resolve(returnVal);
     });
